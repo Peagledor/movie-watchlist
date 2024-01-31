f795b7b79e9cdc61b21bfbb852a441093e74abf3
Jorge Peraza <j.peraza.22@gmail.com>
2024-01-30 22:41:30 -0700

    adds completed part 2 of 5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import MovieScreen from "./components/MovieScreen";
 
 function App() {
+  const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+  const [movieList, setMovieList] = useState([]);
+  const [list, setlist] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getData = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${REACT_APP_API_KEY}&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        console.log(res.data.results);
+        setMovieList(res.data.results);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <MovieScreen
+          List={list} 
+          Page={page}
+          setPage={setPage}
+          MovieList={movieList}
+        />
+      </main>
     </div>
   );
 }
